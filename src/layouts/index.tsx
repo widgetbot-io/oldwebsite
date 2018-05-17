749d5708ed2c820c3191f2385134f1b66e4f8da7
samdd <sam@samdd.me>
2018-05-17 16:52:41 +0100

    Initial commit

---
+++ src/layouts/index.tsx
@@ -0,0 +1,41 @@
+import './index.css'
+
+import * as React from 'react'
+import Helmet from 'react-helmet'
+
+import Header from '../components/header'
+
+const Layout = ({ children, data }) => (
+  <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+export default Layout
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
