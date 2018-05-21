82819dfe587f7b35eefd8c28194b302fa700bc2b
samdd <sam@samdd.me>
2018-05-21 23:43:10 +0100

    Footer

--- src/components/Footer/elements.ts
+++ src/components/Footer/elements.ts
@@ -1,15 +1,18 @@
+import Link from 'gatsby-link'
+
 import styled, { css } from '../../modules/emotion'
 
 export const Root = styled('footer')`
+  display: flex;
+  flex-direction: column;
   position: absolute;
   bottom: 0;
   left: 0;
   width: 100%;
-  height: 150px;
+  height: 200px;
   background-color: #111213;
   box-shadow: 0px -1px 54px rgba(0, 0, 0, 0.3);
-  transform: translateY(100px);
-  padding: 20px 30px;
+  transform: translateY(150px);
   transition: opacity 0.2s ease;
 
   ${({ theme }) =>
@@ -17,4 +20,75 @@ export const Root = styled('footer')`
     css`
       opacity: 0;
     `};
+`
+
+export const Links = styled('ul')`
+  flex-grow: 1;
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  padding: 20px 70px;
+`
+
+interface ISection {
+  name: string
+}
+
+export const Section = styled<ISection, 'li'>('li')`
+  flex-grow: 1;
+
+  &::before {
+    display: block;
+    content: "${({ name }) => name}";
+    font-size: 1.125rem;
+    font-weight: 400;
+    margin: 0px 0px 1rem;
+    color: #90a3e6;
+    text-shadow: rgba(144, 163, 230, 0.12) 0px 0px 3px;
+  }
+`
+
+export namespace Place {
+  const Base = css`
+    display: block;
+    text-decoration: none;
+    opacity: 0.6;
+    transition: opacity 0.2s ease;
+    margin-bottom: 0.4rem;
+
+    &:hover {
+      opacity: 0.9;
+    }
+  `
+
+  export const Route = styled(Link)`
+    ${Base};
+  `
+
+  export const Href = styled('a')`
+    ${Base};
+  `
+}
+
+export const Chin = styled('div')`
+  display: flex;
+  height: 30px;
+  padding: 0 70px;
+  line-height: 30px;
+  font-size: 0.8rem;
+  background-color: rgba(0, 0, 0, 0.1);
+`
+
+export const Copyright = styled('span')`
+  opacity: 0.3;
+`
+
+export const Credits = styled('span')`
+  flex-grow: 1;
+  text-align: right;
+
+  a {
+    color: #a8bdff;
+  }
 `
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,50 @@
 import * as React from 'react'
 
-import { Root } from './elements'
+import {
+  Chin,
+  Copyright,
+  Credits,
+  Links,
+  Place,
+  Root,
+  Section
+} from './elements'
 
-const Footer = () => <Root>FOOTER</Root>
+const Footer = () => (
+  <Root>
+    <Links>
+      <Section name="WidgetBot">
+        <Place.Href href="https://status.widgetbot.io" target="_blank">
+          Server status
+        </Place.Href>
+      </Section>
+      <Section name="About">
+        <Place.Href href="https://github.com/widgetbot-io" target="_blank">
+          GitHub
+        </Place.Href>
+        <Place.Href href="https://discord.gg/zyqZWr2" target="_blank">
+          Discord server
+        </Place.Href>
+        <Place.Href href="mailto:hello-widgetbot@samdd.me" target="_blank">
+          Contact us
+        </Place.Href>
+      </Section>
+      <Section name="Legal">
+        <Place.Route to="/legal/privacy-policy/">Privacy</Place.Route>
+        <Place.Route to="/legal/terms-and-conditions/">
+          Terms and conditions
+        </Place.Route>
+      </Section>
+    </Links>
+    <Chin>
+      <Copyright
+      >{`© ${new Date().getFullYear()} WidgetBot. Not affiliated with Discord`}</Copyright>
+      <Credits>
+        By <a href="https://samdd.me">Sam Denty</a> and{' '}
+        <a href="https://voakie.com">Voakie</a>
+      </Credits>
+    </Chin>
+  </Root>
+)
 
 export default Footer
