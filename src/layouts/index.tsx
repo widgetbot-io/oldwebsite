eb974b1be7ea562d6d401dc950c4ad9e9279d507
Advaith <advaithj1@gmail.com>
2019-10-24 10:41:30 -0700

    beta

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -33,7 +33,7 @@ if (typeof window !== 'undefined') {
         crate = new (window as any).Crate({
           server: '299881420891881473',
           channel: '355719584830980096',
-          shard: 'https://disweb.dashflo.net',
+          shard: 'https://e.widgetbot.io',
           css: css`
             * {
               font-family: Whitney, 'Helvetica Neue', Helvetica, Arial,
